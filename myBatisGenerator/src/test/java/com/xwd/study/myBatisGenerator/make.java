a873ba3b1b2f3eafe46058169f27d294136ee35a
x <xwd@xiaowudong>
2016-12-19 14:10:54 +0800

    新增

---
+++ myBatisGenerator/src/test/java/com/xwd/study/myBatisGenerator/make.java
@@ -0,0 +1,9 @@
+package com.xwd.study.myBatisGenerator;
+
+public class make {
+
+	public static void main(String[] args) {
+		org.mybatis.generator.api.ShellRunner.main(new String[] { "-configfile", "G:/git/codeMachine/myBatisGenerator/src/main/resources/generatorConfig.xml", "-overwrite" });
+	}
+
+}
